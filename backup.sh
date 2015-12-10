fa94b531e4d15665d1e9ebd69f33cffeae09cedd
Matthew Bentley <matthew.t.bentley@gmail.com>
2015-12-09 20:48:56 -0500

    Add backup script

---
+++ backup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source ./vars
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+if [[ $1 == "" ]]; then
+    echo "You must specify \"full\" or \"incremental\" as the first argument"
+    exit 1
+fi
+
+if [[ $1 == "remove-all-but-n-full" ]]; then
+    if [[ $2 == "" ]]; then
+        echo "You must specify a number if using \"remove-all-but-n-full\""
+        exit 1;
+    fi
+    num=$2
+fi
+
+type=$1
+
+echo $duplicity
+
+for location in "${!backup[@]}"; do
+
+    if [ $type == "full" -o $type == "incremental" ]; then
+        echo "Backing up ${backup["$location"]} to $location"
+        $duplicity $type "${backup["$location"]}" "b2://${account}@${bucket}/${location}"
+    fi
+
+    if [[ $type == "remove-all-but-n-full" ]]; then
+        echo "Removing all but $num from $location"
+        $duplicity $type $num --force "b2://${account}@${bucket}/${location}"
+    fi
+
+done
